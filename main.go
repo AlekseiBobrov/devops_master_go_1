7e4901931783241822384b492da270e05764c07d
Aleksei <alexeybobrov@gmail.com>
2026-01-18 19:55:39 +0300

    test

--- main.go
+++ main.go
@@ -43,7 +43,9 @@ requestLabel:
 			}
 			numbers[n] = parsedNumber
 		}
-
+		fmt.Println(string(body))
+		fmt.Println(values)
+		fmt.Println(numbers)
 		if numbers[0] > 30 {
 			fmt.Printf("Load Average is too high: %d\n", numbers[0])
 		}
