d49d675790bff888e57fd96cf535e61060577b3a
Aleksei <alexeybobrov@gmail.com>
2026-01-18 17:34:15 +0300

    first iteration

---
+++ go.mod
@@ -0,0 +1,3 @@
+module devops_master_go_1
+
+go 1.25.6
--- main.go
+++ main.go
@@ -1,3 +1,54 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	i := 0
+requestLabel:
+	for i < 4 {
+		i++
+		response, err := http.Get(" http://srv.msk01.gigacorp.local/_stats")
+		body, err := io.ReadAll(response.Body)
+		if err != nil || response.StatusCode != 200 {
+			continue
+		}
+
+		values := strings.Split(string(body), ",")
+		if len(values) != 7 {
+			continue
+		}
+		numbers := make([]int, len(values))
+		for n, val := range values {
+			parsed_number, err := strconv.Atoi(val)
+			if err != nil {
+				continue requestLabel
+			}
+			numbers[n] = parsed_number
+		}
+
+		if numbers[0] > 30 {
+			fmt.Printf("Load Average is too high: %d\n", numbers[0])
+		}
+
+		if usage := float64(numbers[2]) / float64(numbers[1]); usage > float64(0.8) {
+			fmt.Printf("Memory usage too high: %d%\n", usage*100)
+		}
+
+		if float64(numbers[4])/float64(numbers[3]) > float64(0.9) {
+			fmt.Printf("Free disk space is too low: %d Mb left\n", (numbers[3]-numbers[4])/1024/1024)
+		}
+
+		if float64(numbers[6])/float64(numbers[5]) > float64(0.9) {
+			fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", (numbers[3]-numbers[4])/1024/1024)
+		}
+	}
+	if i == 4 {
+		fmt.Println("Unable to fetch server statistic")
+	}
+}
