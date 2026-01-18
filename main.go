915d2c6b41cb652ff9c263a5478404d1f5b3d065
Aleksei <alexeybobrov@gmail.com>
2026-01-18 19:26:24 +0300

    7 iteration

--- main.go
+++ main.go
@@ -41,16 +41,16 @@ requestLabel:
 			fmt.Printf("Load Average is too high: %d\n", numbers[0])
 		}
 
-		if usage := float64(numbers[2]) / float64(numbers[1]); usage > float64(0.8) {
-			fmt.Printf("Memory usage too high: %f%%\n", usage*100)
+		if usage := (numbers[2] / numbers[1]) * 100; usage > 80 {
+			fmt.Printf("Memory usage too high: %d%%\n", usage)
 		}
 
-		if float64(numbers[4])/float64(numbers[3]) > float64(0.9) {
-			fmt.Printf("Free disk space is too low: %d Mb left\n", (numbers[3]-numbers[4])/1024/1024)
+		if (numbers[4]/numbers[3])*100 > 90 {
+			fmt.Printf("Free disk space is too low: %d Mb left\n", (numbers[3]-numbers[4])/1000/1000)
 		}
 
-		if float64(numbers[6])/float64(numbers[5]) > float64(0.9) {
-			fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", (numbers[3]-numbers[4])/1024/1024)
+		if (numbers[6]/numbers[5])*100 > 90 {
+			fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", (numbers[5]-numbers[6])/1000/1000)
 		}
 		break
 	}
